2d05c4ad278011985e5fa4250a734df8e154d951
koheinthurako <captainthura99@gmail.com>
2023-08-17 23:55:34 +0630

    add blur

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const sidebar = document.getElementById('sidebar');
 const showSideBar = document.getElementById('showSideBar');
+const pageview = document.querySelector('.pageview');
 
 showSideBar.addEventListener('click', function() {
   sidebar.classList.toggle('show');
+  if(sidebar.classList.contains("show")) {
+    pageview.classList.add("blur");
+  } else {
+    pageview.classList.remove("blur");
+  }
 });

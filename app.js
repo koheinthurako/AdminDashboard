6563f74efdabb063057b2958772bff3be9023c5c
koheinthurako <captainthura99@gmail.com>
2023-08-18 23:31:12 +0630

    add user js

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const sidebar = document.getElementById('sidebar');
 const showSideBar = document.getElementById('showSideBar');
-const pageview = document.querySelector('.pageview');
+const userBox = document.getElementById("userBox");
+const userBtn = document.getElementById("userBtn");
+const adminBox = document.getElementById("adminBox");
 
 showSideBar.addEventListener('click', function() {
   sidebar.classList.toggle('show');
   if(sidebar.classList.contains("show")) {
-    pageview.classList.add("blur");
+    adminBox.classList.add("blur");
   } else {
-    pageview.classList.remove("blur");
+    adminBox.classList.remove("blur");
   }
 });
+
+function showUserBox() {
+  if(adminBox.classList.contains("show")) {
+    adminBox.classList.replace("show", "hide");
+  }
+  userBox.classList.replace("hide", "show");
+}
+
+userBtn.addEventListener('click', showUserBox);
